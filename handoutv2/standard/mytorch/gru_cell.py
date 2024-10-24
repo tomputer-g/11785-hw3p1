d5d0c72d1ffbb9ee4c4312e1e2ba621340c2fa42
zimingg <zimingg@umich.edu>
2024-10-24 16:38:44 -0400

    GRU backward almost workin

--- handoutv2/standard/mytorch/gru_cell.py
+++ handoutv2/standard/mytorch/gru_cell.py
@@ -131,10 +131,68 @@ class GRUCell(object):
         """
         # 1) Reshape self.x and self.hidden to (input_dim, 1) and (hidden_dim, 1) respectively
         #    when computing self.dWs...
+        x_t = self.x.reshape((self.d, 1)) #input dim = input size = self.d
+        h_t_prev = self.hidden.reshape((self.h, 1)) # hidden dim = hidden size = self.h
         # 2) Transpose all calculated dWs...
+        # print(delta.shape) #2,
         # 3) Compute all of the derivatives
+        def pp(name: str, var):
+            print(name + " is shape " + str(var.shape))
+        # print(self.d) #5
+        dldh = delta
+        # pp("dldh", dldh) #2,
+        dldz = dldh * (self.hidden - self.n).T
+        # pp("dldz", dldz) #2,
+        dldn = dldh * (np.ones_like(self.z) - self.z).T
+        # pp("dldn", dldn) #2,
+        dldn_act = self.h_act.backward(dldn)
+        # pp("dldn_a", dldn_act) #2,
+        self.dWnx = (dldn_act * x_t).T
+        self.dbnx = dldn_act
+        dldr = dldn_act.reshape(self.h,1) * (self.Wnh @ h_t_prev + self.bnh.reshape(self.h, 1))
+        dldr = dldr.reshape(self.h,)
+        # pp("dldr", dldr)
+        
+        # assert dldr.shape == (self.h,)
+        # pp("Wnh", self.Wnh)
+        # pp("htprev", h_t_prev)
+        # pp("bnh", self.bnh)
+        # pp("dldr", dldr)
+        self.dWnh = (dldn_act * (self.r.T * h_t_prev)).T
+        self.dbnh = dldn_act * (self.r.T)
+        
+        dldz_act = self.z_act.backward(dldz)
+        self.dWzx = (dldz_act * x_t).T
+        self.dbzx = dldz_act
+        self.dWzh = (dldz_act * h_t_prev).T
+        self.dbzh = dldz_act
+
+        dldr_act = self.r_act.backward(dldr)
+        assert dldr_act.shape == (self.h,)
+        self.dWrx = (dldr_act * x_t).T
+        self.dbrx = dldr_act
+        self.dWrh = (dldr_act * h_t_prev).T
+        self.dbrh = dldr_act
+        
+        #SANITY CHECK SIZE
+        assert self.dWrx.shape == (self.h, self.d)
+        assert self.dWzx.shape == (self.h, self.d)
+        assert self.dWnx.shape == (self.h, self.d)
+        assert self.dWrh.shape == (self.h, self.h)
+        assert self.dWzh.shape == (self.h, self.h)
+        assert self.dWnh.shape == (self.h, self.h)
+        assert self.dbrx.shape == (self.h,)
+        assert self.dbzx.shape == (self.h,)
+        assert self.dbnx.shape == (self.h,)
+        assert self.dbrh.shape == (self.h,)
+        assert self.dbzh.shape == (self.h,)
+        assert self.dbnh.shape == (self.h,)
+        
+        dx = dldn_act @ self.Wnx + dldz_act @ self.Wzx + dldr_act @ self.Wrx
+        dh_prev_t = dldh * self.z + dldn_act @ (self.r * self.Wnh) + dldz_act @ self.Wzh + dldr_act @ self.Wrh
         # 4) Know that the autograder grades the gradients in a certain order, and the
         #    local autograder will tell you which gradient you are currently failing.
+        
 
         # ADDITIONAL TIP:
         # Make sure the shapes of the calculated dWs and dbs  match the
@@ -143,5 +201,4 @@ class GRUCell(object):
         assert dx.shape == (self.d,)
         assert dh_prev_t.shape == (self.h,)
 
-        # return dx, dh_prev_t
-        raise NotImplementedError
+        return dx, dh_prev_t
